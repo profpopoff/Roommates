d283e60733e1d6e1f640eb589cf6e9c61a7098e5
Aleksei Ivanov <Profpopoff@gmail.com>
2022-07-25 22:11:41 +0300

    created filters redux storage

---
+++ redux/filtersSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const filtersSlice = createSlice({
+    name: "filters",
+    initialState: {
+        price: { min: 1, max: 1000000 },
+        type: ["bed", "room", "flat", "house", "townhouse"],
+        floor: { min: 1, max: 100 },
+        rooms: null,
+        area: null,
+        conviniences: null
+    },
+    reducers: {
+        setFilers: (state, action) => {
+            state.price = action.payload.price
+            state.type = action.payload.type
+            state.floor = action.payload.floor
+            state.rooms = action.payload.rooms
+            state.area = action.payload.area
+            state.conviniences = action.payload.conviniences
+        },
+        resetFilters: (state) => {
+            state.price = { min: 1, max: 1000000 }
+            state.type = [bed, room, flat, house, townhouse]
+            state.floor = { min: 1, max: 100 }
+            state.rooms = null
+            state.area = null
+            state.conviniences = null
+        }
+    }
+})
+
+export const { setFilers, resetFilters } = filtersSlice.actions;
+
+export default filtersSlice.reducer;
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
+import filtersReducer from './filtersSlice'
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        filters: filtersReducer
     }
 })
