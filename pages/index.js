270f361898cf1cea9dd0b28d4b1d60d025b44114
Aleksei Ivanov <Profpopoff@gmail.com>
2022-07-26 13:10:28 +0300

    minor format changes to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.scss'
 
 import Layout from '../components/Layout'
@@ -8,17 +5,7 @@ import Filters from '../components/Filters/Filters'
 import Posts from '../components/Posts/Posts'
 import Map from '../components/Map/Map'
 
-export default function Home({apartments}) {
-
-  // const [apartments, setApartments] = useState([])
-
-  // useEffect(() => {
-  //   fetch("/api/apartments")
-  //     .then(response => response.json())
-  //     .then(data => setApartments(data))
-  // }, [])
-  console.log(apartments)
-
+export default function Home({ apartments }) {
   return (
     <Layout>
       <div className={styles.container}>
@@ -30,12 +17,8 @@ export default function Home({apartments}) {
   )
 }
 
-// This gets called on every request
 export async function getServerSideProps() {
-  // Fetch data from external API
   const res = await fetch('http://localhost:3000/api/apartments')
   const data = await res.json()
-
-  // Pass data to the page via props
   return { props: { apartments: data } }
 }
