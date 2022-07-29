fa70ff8e227eb89fe8b227296a81ac790becc206
Aleksei Ivanov <Profpopoff@gmail.com>
2022-07-29 22:15:26 +0300

    added image slider

--- pages/apartment/[id].jsx
+++ pages/apartment/[id].jsx
@@ -2,33 +2,76 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/pages/Apartment.module.scss'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhone, faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faComments } from '@fortawesome/free-regular-svg-icons'
+
 import Layout from '../../components/Layout'
 import FavButton from '../../components/FavButton/FavButton'
+import { useState } from 'react'
 
 export default function Apartment({ apartment }) {
-
   return (
     <Layout title={apartment.title}>
       <div className={styles.container}>
         <div className={styles.info}>
-          <Images />
+          <Images images={apartment.images} />
           <Headline title={apartment.title} />
           <Landlord />
           <FavButton />
           <Conveniences />
-          <Stats/>
-          <Desc/>
+          <Stats />
+          <Desc />
         </div>
-        <Reviews/>
+        <Reviews />
       </div>
     </Layout>
   )
 }
 
-const Images = () => {
+const Images = (props) => {
+
+  const [activeImage, setActiveImage] = useState(0)
+
+  console.log(props.images[activeImage])
+
   return (
     <div className={styles.images}>
-      images
+      <div className={styles.active}>
+        <Image
+          className={styles.src}
+          src={props.images[activeImage]}
+          alt=""
+          layout="fill"
+          onClick={activeImage === props.images.length - 1 ? () => setActiveImage(0) : () => setActiveImage(activeImage + 1)}
+        />
+        <button
+          className={`${styles.imgButton} ${styles.next}`}
+          onClick={activeImage === props.images.length - 1 ? () => setActiveImage(0) : () => setActiveImage(activeImage + 1)}
+        >
+          <FontAwesomeIcon icon={faAngleRight} className="icon" />
+        </button>
+        <button
+          className={`${styles.imgButton} ${styles.prev}`}
+          onClick={activeImage === 0 ? () => setActiveImage(props.images.length - 1) : () => setActiveImage(activeImage - 1)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} className="icon" />
+        </button>
+      </div>
+      <div className={styles.inactive}>
+        {props.images.map((image, index) => (
+          <div className={styles.image}>
+            <Image
+              key={index}
+              className={styles.src}
+              src={image}
+              alt=""
+              layout="fill"
+              onClick={() => setActiveImage(index)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
