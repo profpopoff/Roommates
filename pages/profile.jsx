06e97cc14eef0b0de7e2b988efecd7054b079f1f
Aleksei Ivanov <profpopoff@gmail.com>
2022-08-13 21:39:59 +0300

    started profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,14 +1,44 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Profile.module.css'
+import Router from 'next/router'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import styles from '../styles/pages/Profile.module.scss'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone, faPenToSquare, faComments, faHeart, faBuilding, faHouseChimney } from '@fortawesome/free-solid-svg-icons'
+
 import Layout from '../components/Layout'
 
+
+// todo: redirect from here if not authorzed
 export default function Profile() {
+
+  const user = useSelector((state) => state.user.info)
+
   return (
-    <Layout title="USER NAME VVEDI PLZ">
-      <div>
-        profile
+    <Layout title={`${user?.name} ${user?.surname}`}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Picture />
+        </div>
       </div>
     </Layout>
+  )
+}
+
+const Picture = () => {
+
+  const user = useSelector((state) => state.user.info)
+
+  const [editActive, setEditActive] = useState(false)
+
+  return (
+    <div className={styles.image}>
+      <Image src={user?.image ? user.image : '/img/default-user.png'} alt="user profile picture" layout='fill' />
+      <button className={styles.editBtn} onClick={() => { setEditActive(true) }}>
+        <FontAwesomeIcon icon={faPenToSquare} /> <span className="sr-only">Edit profile</span>
+      </button>
+    </div>
   )
 }
