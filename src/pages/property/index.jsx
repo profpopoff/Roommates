aa76439ca2548e8ac23e006560a74f704ff954fc
Aleksei Ivanov <profpopoff@gmail.com>
2022-08-19 16:46:22 +0300

    added property data fetch

--- src/pages/property/index.jsx
+++ src/pages/property/index.jsx
@@ -9,8 +9,10 @@ import { wrapper } from '../../redux/store'
 import { getUser } from '../api/users/[id]'
 import { setUser } from '../../redux/slices/user'
 import Layout from '../../components/Layout'
+import { getApartment } from '../api/apartments/[id]'
+import { jsonParser } from '../../utils/functions'
 
-export default function Property() {
+export default function Property({properties}) {
   return (
     <Layout title="My Property">
       <div className={styles.container}>
@@ -29,5 +31,14 @@ export const getServerSideProps = wrapper.getServerSideProps(store => async ({ r
     const decodedToken = jwt.decode(token)
     const user = await getUser(decodedToken.id)
     store.dispatch(setUser(JSON.parse(JSON.stringify(user))))
+    const propertyIds = store.getState().user.info.property
+    if (propertyIds.length) {
+      const properties = await Promise.all(
+        propertyIds.map(propertyId => (
+          getApartment(propertyId)
+        ))
+      )
+      return { props: { properties: jsonParser(properties) } }
+    }
   }
 })
