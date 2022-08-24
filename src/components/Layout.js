211b9e3d992b4f8a8758260334a9f9b1084de43a
Aleksei Ivanov <profpopoff@gmail.com>
2022-08-24 15:32:13 +0300

    deleted unused code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,27 +2,7 @@ import Head from 'next/head'
 
 import Header from './Header/Header'
 
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import jwt from 'jsonwebtoken'
-import cookie from 'cookie'
-import { setUser } from '../redux/slices/user'
-
 export default function Layout({ children, title }) {
-
-  const dispatch = useDispatch()
-
-  // this thing sets user info if we have a cookie token
-  // useEffect(() => {
-  //   let token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-  //   if (token) {
-  //     const decodedToken = jwt.decode(token)
-  //     fetch(`/api/users/${decodedToken.id}`)
-  //       .then(response => response.json())
-  //       .then(user => dispatch(setUser(user)))
-  //   }
-  // }, [])
-
   return (
     <>
       <Head>
