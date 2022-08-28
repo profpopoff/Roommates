bb2170477c3a18151b81d53ba2c6755899931f12
Aleksei Ivanov <profpopoff@gmail.com>
2022-08-28 18:02:28 +0300

    created chat ui

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Chat.module.css'
+import { useState } from 'react'
+import styles from '../styles/pages/Chat.module.scss'
 
 import * as cookie from 'cookie'
 import jwt from 'jsonwebtoken'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsis, faTrashCan, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { format } from 'timeago.js'
 
 import { wrapper } from '../redux/store'
 import { getUser } from './api/users/[id]'
@@ -13,28 +17,88 @@ import CustomInput from '../components/CustomInput/CustomInput'
 import CustomTextarea from '../components/CustomTextArea/CustomTextArea'
 import CustomToggle from '../components/CustomToggle/CustomToggle'
 import { jsonParser } from '../utils/functions'
+import Modal from '../components/Modal/Modal'
 
 export default function Chat() {
   return (
     <Layout title="Chat">
-      <div>
-        Chat
-        <CustomInput
-          name="email"
-          label="Почта"
-          type="number"
-          value="123"
-          handleChange={(e) => console.log(e.target.value)}
-        />
-        <CustomTextarea
-          label="Описание"
-          name="desc"
-          value={321}
-          handleChange={(e) => console.log(e.target.value)}
-        />
-        <CustomToggle name='кухня' label="Кухня" checked={true} onChange={(e) => console.log(e.target.value, e.target.checked)} />
+      <div className={styles.container}>
+        <div className={styles.conversations}>
+          <h2>Доступные собеседники</h2>
+          <Conversation />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+        </div>
+        <Box />
       </div>
     </Layout>
+  )
+}
+
+const Conversation = () => {
+
+  const [conversationMenuActive, setConversationMenuActive] = useState(false)
+
+  return (
+    <div className={styles.conversation}>
+      <div className={styles.user}>
+        <div className={styles.image}>
+          <Image className={styles.src} src={'/img/cover.jpeg'} alt="user profile picture" layout='fill' />
+          <span className={styles.notification}>1</span>
+        </div>
+        <div className={styles.name}>Алексей Иванов</div>
+      </div>
+      <button className={styles.conversationMenuBtn} onClick={() => setConversationMenuActive(true)}>
+        <FontAwesomeIcon icon={faEllipsis} />
+      </button>
+      <ConversationMenu active={conversationMenuActive} setActive={setConversationMenuActive} />
+    </div>
+  )
+}
+
+const ConversationMenu = (props) => {
+  return (
+    <Modal active={props.active} setActive={props.setActive}>
+      <h3>text</h3>
+    </Modal>
+  )
+}
+
+const Box = () => {
+  return (
+    <div className={styles.box}>
+      <h2 className={styles.name}>Алексей Иванов</h2>
+      <div className={styles.messages}>
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+      </div>
+    </div>
+  )
+}
+
+const Message = () => {
+  return (
+    <div className={`${styles.message} ${styles.own}`}>
+      <div className={styles.body}>
+        <div className={styles.image}>
+          <Image className={styles.src} src={'/img/cover.jpeg'} alt="user profile picture" layout='fill' />
+        </div>
+        <div className={styles.text}>
+          Hello! How are youy? 
+          {/* <div className={styles.time}>{format(props.message.createdAt)}</div>  */}
+          <div className={styles.time}>{format('2022-07-22T12:27:43.388Z')}</div> 
+        </div>
+      </div>
+    </div>
   )
 }
 
