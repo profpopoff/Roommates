5146e3e729cdbc23ef8b4727edf4a5ca0dadca7a
Aleksei Ivanov <profpopoff@gmail.com>
2022-08-30 12:07:00 +0300

    changed favourite button ui

--- src/components/FavButton/FavButton.jsx
+++ src/components/FavButton/FavButton.jsx
@@ -1,17 +1,46 @@
 import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import styles from './FavButton.module.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
 
-export default function FavButton() {
+import { addFavourite, deleteFavourite } from '../../redux/slices/favourites'
+
+export default function FavButton({ id }) {
+
+    const favourites = useSelector((state) => state.favourites.posts)
+    const dispatch = useDispatch()
 
     const [isFavourite, setIsFavourite] = useState(false)
 
+    const handleToggle = async (e) => {
+        if (e.target.checked) {
+            setIsFavourite(true)
+            dispatch(addFavourite(e.target.value))
+        } else {
+            setIsFavourite(false)
+            dispatch(deleteFavourite(favourites.filter(item => item !== e.target.value)))
+        }
+    }
+
     return (
-        <button className={styles.favButton} onClick={() => setIsFavourite(!isFavourite)}>
-            {isFavourite ? <FontAwesomeIcon icon={faHeartSolid} /> : <FontAwesomeIcon icon={faHeartRegular} />}
-        </button>
+
+        <div className={styles.container}>
+            <label className={styles.label}
+                htmlFor={id}
+            >
+                {isFavourite ? <FontAwesomeIcon icon={faHeartSolid} /> : <FontAwesomeIcon icon={faHeartRegular} />}
+            </label>
+            <input
+                className={styles.toggle}
+                type="checkbox"
+                id={id}
+                // defaultChecked={props.checked}
+                value={id}
+                onClick={handleToggle}
+            />
+        </div>
     )
 }
