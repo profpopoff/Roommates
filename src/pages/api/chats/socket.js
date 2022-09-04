2157054cbbb16ce789d13dd5fbc1883eb2f9d73d
Aleksei Ivanov <profpopoff@gmail.com>
2022-09-04 12:51:59 +0300

    socket io (doesnt work)

--- src/pages/api/chats/socket.js
+++ src/pages/api/chats/socket.js
@@ -1,5 +1,4 @@
-import { Server } from "socket.io"
-import messageHandler from "../../utils/sockets/messageHandler"
+import { Server, Socket } from "socket.io"
 
 export default function SocketHandler(req, res) {
    // It means that socket server was already initialised
@@ -12,17 +11,45 @@ export default function SocketHandler(req, res) {
    const io = new Server(res.socket.server)
    res.socket.server.io = io
 
-   const onConnection = (socket) => {
-      const createdMessage = (msg) => {
-         socket.broadcast.emit("newIncomingMessage", msg)
-      }
+   // const onConnection = (socket) => {
+      // const createdMessage = (msg) => {
+      //    socket.broadcast.emit("newIncomingMessage", msg)
+      // }
 
-      socket.on("createdMessage", createdMessage)
-   }
+      // socket.on("createdMessage", createdMessage)
+   // console.log("onConnection")
 
-   // Define actions inside
-   io.on("connection", onConnection)
+   // }
 
-   console.log("Setting up socket")
+   // // Define actions inside
+   // io.on("connection", onConnection)
+
+   // console.log("Setting up socket")
+
+   io.on('connection', (socket) => {
+      console.log('connected to socket.io')
+
+      socket.on('setup', (userId) => {
+         socket.join(userId)
+         console.log(userId)
+         socket.emit('connected')
+      })
+
+      socket.on('join chat', (room) => {
+         socket.join(room)
+         console.log('user joined room: ' + room)
+      })
+
+      // socket.on('new message', (newMessageRecieved) => {
+      //    if (!newMessageRecieved.members) return console.log('chat.users not defined')
+      //    newMessageRecieved.members.forEach(user => {
+      //       if (user !== newMessageRecieved.sender) {
+      //          socket.in(user).emit('message recieved', newMessageRecieved)
+      //       }
+      //       else { return }
+
+      //    })
+      // })
+   })
    res.end()
 }
