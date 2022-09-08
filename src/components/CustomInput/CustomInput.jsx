edab1fcc6f06e61dfef1724318d37cca865e4c2e
Aleksei Ivanov <profpopoff@gmail.com>
2022-09-08 16:55:35 +0300

    added password input type toggle

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,15 +1,21 @@
 import styles from './CustomInput.module.scss'
 import { useState } from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
 export default function CustomInput(props) {
 
     const [text, setText] = useState(props.value ? props.value : '')
+
+    const [type, setType] = useState(props.type)
 
     return (
         <div className={styles.container}>
             <input
                 className={styles.input}
-                type={props.type}
+                type={type}
+                initialType={props.type}
                 name={props.name}
                 id={props.name}
                 placeholder={props.label}
@@ -18,6 +24,11 @@ export default function CustomInput(props) {
                 autoComplete="off"
             />
             <label className={styles.label} htmlFor={props.name}>{props.label}</label>
+            {props.type === 'password' &&
+                <button className={styles.toggleType} type="button" onClick={type === 'password' ? () => setType('text') : () => setType('password')} >
+                    <FontAwesomeIcon icon={type === 'password' ? faEyeSlash : faEye} />
+                </button>
+            }
         </div>
     )
 }
