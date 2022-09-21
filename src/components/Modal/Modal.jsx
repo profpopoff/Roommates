b966bfcc1e653002858c1dfc8ff5a211e62f3d6f
Aleksei Ivanov <profpopoff@gmail.com>
2022-09-21 19:41:57 +0300

    adapted for smaller resolution

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,13 @@
 import styles from './Modal.module.scss'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
 export default function Modal({ active, setActive, children }) {
    return (
       <div className={`${styles.container} ${active && styles.active}`} onClick={() => setActive(false)}>
          <div className={`${styles.content} ${active && styles.active}`} onClick={e => e.stopPropagation()}>
+            <button className={styles.closeBtn} onClick={() => setActive(false)}><FontAwesomeIcon icon={faXmark} /></button>
             {children}
          </div>
       </div>
