c0e90ddaf3c150f324dfa0e9d015792d1521baed
Aleksei Ivanov <profpopoff@gmail.com>
2022-09-22 21:24:56 +0300

    added  console log

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -17,6 +17,7 @@ export const useHttp = () => {
          const response = await fetch(url, { method, body, headers })
 
          const data = await response.json()
+         console.log(data)
 
          if (!response.ok) {
             throw new Error(data.message || 'Something went wrong...')
